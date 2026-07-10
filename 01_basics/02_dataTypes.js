0428df744f55357f60905c45e83615dcaa185fae
rohitkushwahtech26 <hirerohitkushwah@gmail.com>
2026-07-10 13:01:14 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello Rohit") // we are using node.js, not browser
+
+console.log(3 
+    
+    +
+     3) // code readability should be high
+
+console.log("Hello Rohit")
+
+// Primitive Data Type //
+
+let Name = "Rohit" //string data type
+let Age = 23 // number data type
+             // bigint-> it is uesd for large number of number data
+let isLoggedIN = true // boolean data type true/false
+let state; // undefined data type -> it helps to create variable without values.
+let Mobile_No = null // null-> standalone value -it helps for insert null value.
+
+// symbol -> it helps for uniqueness
+
+/*................................................................................*/
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
