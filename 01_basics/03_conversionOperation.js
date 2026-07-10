2623fb008d5c1afa7e4c74ac686623f77982b742
rohitkushwahtech26 <hirerohitkushwah@gmail.com>
2026-07-10 13:40:28 +0000

    ConversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+let score = "33"
+// let score_a = "33aa"
+// let score_2 = null
+// let score_3 = undefined
+// let score_4 = true
+// let score_5 = "rohit"
+
+
+
+// console.log(typeof (score)) 
+// console.log(typeof score)
+//both are valid
+
+let valueInNumber = Number(score)
+// let valueInNumber_a = Number(score_a)
+// let valueInNumber_2 = Number(score_2)
+// let valueInNumber_3 = Number(score_3)
+// let valueInNumber_4 = Number(score_4)
+// let valueInNumber_5 = Number(score_5)
+
+console.log(typeof valueInNumber)
+
+// console.log(valueInNumber_a)
+// console.log(valueInNumber_2)
+// console.log(valueInNumber_3)
+// console.log(valueInNumber_4)
+// console.log(valueInNumber_5)
+
+
+// "33" --> 33
+//"33aa" --> NaN
+// true /false --> 1/0
+// undefined --> NaN
+// null --> 0
+
+let isLoggedIn = 1
+// let isLoggedIn_1 = ""
+// let isLoggedIn_2 = "Rohit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn_1 = Boolean(isLoggedIn_1)
+// let booleanIsLoggedIn_2 = Boolean(isLoggedIn_2)
+
+console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn_1)
+// console.log(booleanIsLoggedIn_2)
+
+// 1 --> true; 0 --> false;
+//"" --> false
+//"Rohit" --> true
