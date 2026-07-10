39162b140e226454df1dbb279d316b2e0e2ccd2f
rohitkushwahtech26 <hirerohitkushwah@gmail.com>
2026-07-10 13:44:41 +0000

    conversion_update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -48,3 +48,12 @@ console.log(booleanIsLoggedIn)
 // 1 --> true; 0 --> false;
 //"" --> false
 //"Rohit" --> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
+
+
+/*....................................................................................*/
