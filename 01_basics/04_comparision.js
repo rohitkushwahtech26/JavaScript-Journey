d1ca00aaaffb26f47cfef3925702aa6852cd4d45
rohitkushwahtech26 <hirerohitkushwah@gmail.com>
2026-07-12 02:32:18 +0000

    comparion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2">2);
+// console.log(1<"2");
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+
+
+console.log(undefined > 1);
+console.log(undefined < 1);
+console.log(undefined >= 1);
+console.log(undefined <= 1);
+
+// ===
+console.log(null>=0);
+console.log("3"===3);
+console.log("3"==3);
