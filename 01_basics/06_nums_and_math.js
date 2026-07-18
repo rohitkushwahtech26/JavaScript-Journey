bec5a68901ce879fd3bfe1db0d8772ee094285b7
rohitkushwahtech26 <hirerohitkushwah@gmail.com>
2026-07-18 07:59:56 +0000

    math and nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,45 @@
+// /*..................................Nums.................................................*/
+
+// const score = 400 
+// console.log(score);
+
+// const balance = new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().indexOf(0));
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber = 123.86487
+
+// console.log(otherNumber.toPrecision(4));
+// console.log();
+
+
+// const hundreds = 200000000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+/*.........................................Maths...................................*/
+
+
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(5.7));
+console.log(Math.ceil(5.1));
+console.log(Math.floor(5.7));
+console.log(Math.pow(2,3))
+console.log(Math.min(7,5,3,8,3));
+console.log(Math.max(7,5,3,8,3));
+
+console.log(Math.random()*10)
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
